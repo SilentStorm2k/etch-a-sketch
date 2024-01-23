4199d7b2168d7333fc015df8d10e83603d9f81f6
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-23 23:12:20 +0530

    feat: added sliding bar to increase resolution

--- js/index.js
+++ js/index.js
@@ -1,27 +1,33 @@
-const size = 16;
 const inputSize = document.querySelector("#size");
 const resetButton = document.querySelector("#reset");
-const download = document.querySelector("#download");
+const downloadButton = document.querySelector("#download");
 const sketchBoard = document.querySelector(".sketch-container");
 let brightness = 1, dimFactor = 0.1;
 
-resetButton.addEventListener("mouseover", reset);
+resetButton.addEventListener("click", reset);
+inputSize.oninput = function() {
+    reset(event, inputSize.value);
+};
 
 // calling reset to initialize an empty sketch board
 reset();
 
-function reset (event) {
+function reset (event, size = 16) {
+    if (size === 16) {
+        inputSize.value = 16;
+    }
+    brightness = 1;
     while (sketchBoard.firstChild) {
         sketchBoard.removeChild(sketchBoard.lastChild);
     }
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            addPixel();
+            addPixel(size);
         }
     }
 }
 
-function addPixel() {
+function addPixel(size) {
     const newPixel = document.createElement('div');
     newPixel.style.width = (24)/(size) + "rem";
     newPixel.style.height = (24)/(size) + "rem";
@@ -36,7 +42,6 @@ function changeColor(event) {
     const currentPixel = event.target;
     const randomColor = "rgba(" + brightness*getRandom(256) + "," + brightness*getRandom(256) + "," + brightness*getRandom(256) + ")";
     currentPixel.style.backgroundColor = randomColor;
-    currentPixel.style.brightness = brightness;
     event.stopPropagation();
     currentPixel.removeEventListener("mouseover", changeColor);
 }
@@ -44,3 +49,16 @@ function changeColor(event) {
 function getRandom (max) {
     return Math.round(Math.random()*max);
 }
+
+// downloadButton.addEventListener('click', downloadSketch);
+
+// function downloadURI (uri, name) {
+//     const link = document.createElement('a');
+//     link.download = name;
+//     link.href = uri;
+//     link.click()
+// }
+
+// function downloadSketch (event) {
+//     const sketch = sketchBoard.toDataURL("images/temp.png");
+// }
